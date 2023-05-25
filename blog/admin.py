1805dc5318826412087c491148f94228992638bc
vmit2005 <vmit2005@yandex.ru>
2023-05-25 22:38:59 +0400

    25_05_23

---
+++ blog/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from .models import *
+from django.utils.safestring import mark_safe
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+class BlogAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+class BlogAdmin(admin.ModelAdmin):
+    form = BlogAdminForm
+    prepopulated_fields = {"slug": ('title',)}
+    list_display = ('id', 'title', 'cat', 'time_created', 'get_html_photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title__iregex', 'content__iregex')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_created')
+    save_on_top = True
+    fields = ('title','slug','cat','content','photo','get_html_photo','is_published','time_created')
+    readonly_fields = ('get_html_photo','is_published','time_created')
+
+
+
+    def get_html_photo(self,object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width='50'>")
+
+    get_html_photo.short_description='Миниатюра'
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('name',)}
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name__iregex',)
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title= 'Admin панель блога'
+admin.site.site_header= 'Admin панель блога'
